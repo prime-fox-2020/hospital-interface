c2d8351aa4a3249d3298291de92b5691e1aa8318
chabode <rizqa.aulia@gmail.com>
2020-04-08 21:16:44 +0700

    benerin yg typo

--- view.js
+++ view.js
@@ -5,8 +5,7 @@ class View{
         node index.js register [myName] [myPosition] [myUsername] [myPassword]     === khusus employee
         node index.js viewAllEmployee                                              === bisa kapan saja tanpa login
         node index.js login [myUsername] [myPassword]                              === khusus employee
-        node index.js logout                                                       === khusus employee
-        node index.js addPatient [patientName] [sakit1] [sakit2] [sakit3]     === khusus role dokter
+        node index.js addPatient [patientName] [sakit1] [sakit2] [sakit3]          === khusus role dokter
         node index.js viewAllPatient                                               === bisa siapa saja asal login terlebih dahulu
         node index.js logout                                                       === khusus employee
         ===============================`)
