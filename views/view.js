93c35d5e9235d7603156f3182a6abc84a92c311d
susilo hartomo <encusilo545110@gmail.com>
2020-04-08 17:05:26 +0700

    hospital

---
+++ views/view.js
@@ -0,0 +1,22 @@
+class View {
+
+    static err(error) {
+        console.log(error);
+    }
+
+    static message(msg) {
+        console.clear();
+        console.log('SUCCESS');
+        console.log('===========');
+        console.log(msg);
+    }
+    
+    static login(success, msg) {
+        // console.clear();
+        console.log(success);
+        console.log('===========');
+        console.log(msg);
+    }
+}
+
+module.exports = View
