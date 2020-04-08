9cd44ab96c115734bd3963f0c56d9b0a7ec2090e
chabode <rizqa.aulia@gmail.com>
2020-04-08 19:20:14 +0700

    Release 0 - 5: Done

---
+++ view.js
@@ -0,0 +1,22 @@
+
+class View{
+    static viewHelp(){
+        console.log(`        ======== HOSPITAL INTERFACE ========
+        node index.js register [myName] [myPosition] [myUsername] [myPassword]     === khusus employee
+        node index.js viewAllEmployee                                              === bisa kapan saja tanpa login
+        node index.js login [myUsername] [myPassword]                              === khusus employee
+        node index.js logout                                                       === khusus employee
+        node index.js addPatient [id] [patientName] [sakit1] [sakit2] [sakit3]     === khusus role dokter
+        node index.js viewAllPatient                                               === bisa siapa saja asal login terlebih dahulu
+        node index.js logout                                                       === khusus employee
+        ===============================`)
+    }
+    static printError(err){
+        console.log(err)
+    }
+    static printSuccess(data){
+        console.log(data)
+    }
+}
+
+module.exports = View
